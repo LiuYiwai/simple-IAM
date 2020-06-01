29d8ace5f0c574626b4f07f1307d1e31b684f3a0
Liu Heng <liuh012@gmail.com>
2020-06-01 17:24:37 +0800

    fixed bugs that caused some cases of backward failure

--- iam/modules/instance_extent_filling.py
+++ iam/modules/instance_extent_filling.py
@@ -1,7 +1,9 @@
+import collections
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.checkpoint import checkpoint
+import torch.utils.checkpoint as cp
 
 from iam.models.decode_module import DecodeModule
 from iam.models.encode_module import EncodeModule
@@ -16,7 +18,6 @@ class InstanceExtentFilling(nn.Sequential):
 
         self.inference = False
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.iterate_num = iterate_num
         self.channel_num = channel_num
         self.bn_momentum = 0.1
         self.pool_multiple = 2 * 2
@@ -24,6 +25,15 @@ class InstanceExtentFilling(nn.Sequential):
         self.offset = kernel // 2
         self.W = self.H = image_size // self.pool_multiple
         self.norm_sum = 1
+        self.counter = None
+        self.batch_list = None
+
+        self.iterate_num = iterate_num
+        self.sub_iterate_num = sub_iterate_num
+        repeat_num = self.iterate_num // self.sub_iterate_num
+        rest_num = self.iterate_num % self.sub_iterate_num
+        self.iterate_list = [self.sub_iterate_num] * repeat_num
+        self.iterate_list.append(rest_num)
 
         track_running_stats = True
         self.encode = EncodeModule(self.channel_num, self.bn_momentum, track_running_stats)
@@ -32,7 +42,6 @@ class InstanceExtentFilling(nn.Sequential):
                                               self.bn_momentum, track_running_stats)
 
         self.use_checkpoints = use_checkpoints
-        self.sub_iterate_num = sub_iterate_num
 
     def eval(self):
         super(InstanceExtentFilling, self).eval()
@@ -49,27 +58,6 @@ class InstanceExtentFilling(nn.Sequential):
             if isinstance(layer, nn.BatchNorm2d):
                 layer.momentum = self.bn_momentum
 
-    def _iterate_filling(self, inp, peak_list, weight):
-        batch_num = weight.size(0)
-        result = torch.zeros_like(inp)
-
-        unfold_inp = F.unfold(inp, kernel_size=self.kernel, padding=self.offset)
-        for batch_idx in range(batch_num):
-            mask = peak_list[:, 0] == batch_idx
-            if mask.sum().item() == 0:
-                continue
-            temp = unfold_inp[mask].mul(weight[batch_idx])
-            temp = temp.view(temp.shape[0], self.channel_num, self.kernel, self.kernel, self.H, self.W)
-            temp = temp.permute(0, 1, 4, 5, 2, 3).view(temp.shape[0], self.channel_num, self.H, self.W, -1)
-            result[mask] = temp.sum(4)
-
-        return result
-
-    def _iterate_filling_times(self, x, peak_list, weight, times):
-        for i in range(int(times.item())):
-            x = self._iterate_filling(x, peak_list, weight)
-        return x
-
     def _norm_weight(self, weight):
 
         weight_size = weight.size()
@@ -84,31 +72,50 @@ class InstanceExtentFilling(nn.Sequential):
 
         return norm_weight
 
+    def _iterate_filling(self, inp, peak_list, weight):
+
+        result = torch.zeros_like(inp)
+
+        unfold_inp = F.unfold(inp, kernel_size=self.kernel, padding=self.offset)
+        for iter_idx, batch_idx in enumerate(self.batch_list):
+            mask = peak_list[:, 0] == batch_idx
+            if mask.sum().item() == 0:
+                continue
+            temp = unfold_inp[mask].mul(weight[iter_idx])
+            temp = temp.view(temp.shape[0], self.channel_num, self.kernel, self.kernel, self.H, self.W)
+            temp = temp.permute(0, 1, 4, 5, 2, 3).view(temp.shape[0], self.channel_num, self.H, self.W, -1)
+            result[mask] = temp.sum(4)
+
+        return result
+
+    def _iterate_filling_times(self, x, peak_list, weight, times):
+        for i in range(int(times.item())):
+            x = self._iterate_filling(x, peak_list, weight)
+        return x
+
     def forward(self, peak_response_maps, peak_list, p2, p3, p4):
 
-        batch_num = p2.shape[0]
+        self.counter = collections.Counter(peak_list[:, 0].cpu().numpy().tolist())
+        self.batch_list = list(dict(self.counter).keys())
 
         pyramid = self.feature_pyramid(p2, p3, p4)
 
         weight = pyramid.permute(0, 2, 3, 1)
-        weight = weight.view(batch_num, self.H, self.W,
+        weight = weight.view(len(self.batch_list), self.H, self.W,
                              self.channel_num, self.kernel, self.kernel)
         weight = self._norm_weight(weight)
-        weight = weight.view(batch_num, self.H, self.W, -1)
-        weight = weight.permute(0, 3, 1, 2).view(batch_num, weight.shape[3], -1)
+        weight = weight.view(len(self.batch_list), self.H, self.W, -1)
+        weight = weight.permute(0, 3, 1, 2).view(len(self.batch_list), weight.shape[3], -1)
 
         inp = peak_response_maps.unsqueeze(1)
         x = self.encode(inp)
 
         if self.inference == False and self.use_checkpoints:
-            repeat_num = self.iterate_num // self.sub_iterate_num
-            for i in range(repeat_num):
-                x = checkpoint(self._iterate_filling_times, x, peak_list, weight, torch.Tensor([self.sub_iterate_num]))
-            x = checkpoint(self._iterate_filling_times, x, peak_list, weight,
-                           torch.Tensor([self.iterate_num % self.sub_iterate_num]))
+            for it_num in self.iterate_list:
+                x = cp.checkpoint(self._iterate_filling_times, x, peak_list, weight, torch.Tensor([it_num]))
         else:
-            for i in range(self.iterate_num):
-                x = self._iterate_filling(x, peak_list, weight)
+            for it_num in self.iterate_list:
+                x = self._iterate_filling_times(x, peak_list, weight, torch.Tensor([it_num]))
 
         decode_out = self.decode(x)
         decode_out = decode_out.squeeze(1)
